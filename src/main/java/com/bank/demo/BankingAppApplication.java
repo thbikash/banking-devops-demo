ccdd26f5c2bb45af952422516230f1ada8871d03
Bikash Thapa <bikashthazure2025@gmail.com>
2026-01-28 11:43:31 -0500

    Test Version

--- src/main/java/com/bank/demo/BankingAppApplication.java
+++ src/main/java/com/bank/demo/BankingAppApplication.java
@@ -11,7 +11,7 @@ public class BankingAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BankingAppApplication.class, args);
-        System.out.println("Core Banking Application Started");
+        System.out.println("Core Banking Application Started...");
     }
 
     @GetMapping("/health")
