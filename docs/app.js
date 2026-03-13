7f0b47cdadc5bdc840b10448ba7a1e46fc36f04a
Bikash Thapa <bikashthazure2025@gmail.com>
2026-03-13 11:56:57 -0400

    updated

--- docs/app.js
+++ docs/app.js
@@ -1,36 +1,38 @@
-const API="https://deploy-api-342079872292.us-central1.run.app"
+const API = "https://deploy-api-342079872292.us-central1.run.app"
 
 async function loadImages(){
 
- const res=await fetch(API+"/images")
+ const res = await fetch(API + "/images")
 
- const text=await res.text()
+ const images = await res.json()
 
- const lines=text.split("\n")
+ const select = document.getElementById("images")
 
- const select=document.getElementById("images")
+ images.forEach(img => {
 
- lines.forEach(l=>{
-  if(l.includes("banking-app")){
-   let opt=document.createElement("option")
-   opt.value=l
-   opt.text=l
-   select.appendChild(opt)
-  }
+  const option = document.createElement("option")
+
+  option.value = img
+  option.text = img
+
+  select.appendChild(option)
+
  })
+
 }
 
 async function deploy(){
 
- const image=document.getElementById("images").value
+ const image = document.getElementById("images").value
 
- await fetch(API+"/deploy",{
+ await fetch(API + "/deploy",{
   method:"POST",
   headers:{"Content-Type":"application/json"},
-  body:JSON.stringify({image})
+  body: JSON.stringify({image})
  })
 
  alert("Deployment started")
+
 }
 
 loadImages()
