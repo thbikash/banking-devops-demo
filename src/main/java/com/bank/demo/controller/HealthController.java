0c0de7e9beb1e966c44d5aea23b7dbcf40a49f7f
Bikash Thapa <bikashthazure2025@gmail.com>
2026-03-16 22:11:29 -0400

    Code Updated

--- src/main/java/com/bank/demo/controller/HealthController.java
+++ src/main/java/com/bank/demo/controller/HealthController.java
@@ -8,7 +8,7 @@ public class HealthController {
 
     @GetMapping("/")
     public String home() {
-        return "Banking App. is running successfully.....";
+        return "Banking App. is running successfully.....V1.0";
     }
 
     @GetMapping("/health")
